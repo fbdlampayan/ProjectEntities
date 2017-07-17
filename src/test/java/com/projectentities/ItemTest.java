bc839196056ce772f487931b5fdf75c632535788
lampayan <francis.lampayan@nokia.com>
2017-07-17 09:21:42 +0200

    cleanup.

--- src/test/java/com/projectentities/ItemTest.java
+++ src/test/java/com/projectentities/ItemTest.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.nokia.whispernet.projectentities;
+package com.projectentities;
 
+import com.projectentities.Item;
 import java.util.HashSet;
 import org.junit.Test;
 import static org.junit.Assert.*;
